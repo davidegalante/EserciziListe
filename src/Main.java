de4d0b7b34ec1f2cb8f5371af278d15a8cf1e8ee
Davide <davidegalante88@gmail.com>
2020-05-14 18:07:28 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,26 @@ public static void main(String[] args) {
 	l.inserisciTesta(2);
 	l.inserisciTesta(0);
 	l.inserisciTesta(4);
+	l.eliminaDispari();
 	l.stampa();
+	System.out.println("lunghezza: "+l.size());
 	
 	int s = l.eliminaInTesta();
-	System.out.println("ho eliminato: "+s); 	//D
+	System.out.println("\nho eliminato: "+s); 	//D
 	l.stampa();									//C B A
+	System.out.println("lunghezza: "+l.size());
 	s = l.eliminaInTesta();
-	System.out.println("ho eliminato: "+s);		//C
+	System.out.println("\nho eliminato: "+s);		//C
 	l.stampa();									//B A
+	System.out.println("lunghezza: "+l.size());
 	s = l.eliminaInTesta();
-	System.out.println("ho eliminato: "+s);		//B
+	System.out.println("\nho eliminato: "+s);		//B
 	l.stampa();									//A
+	System.out.println("lunghezza: "+l.size());
 	s = l.eliminaInTesta();
-	System.out.println("ho eliminato: "+s);		//A
+	System.out.println("\nho eliminato: "+s);		//A
 	l.stampa();									//null
+	System.out.println("lunghezza: "+l.size());
 	
 	
 	}
