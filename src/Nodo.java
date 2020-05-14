c83e1de7d86106b0ee8fb1a60390f900a91d0641
Davide <davidegalante88@gmail.com>
2020-05-14 18:16:09 +0200

    Update Nodo.java

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,7 +1,7 @@
 /**
  * @author Alberto Ferrari - https://albertoferrari.github.io/
  * @license This software is free - http://www.gnu.org/licenses/gpl.html
- * Nodo di una lista di stringhe
+ * Nodo di una lista di stringhe//
  */
 public class Nodo {
 	
