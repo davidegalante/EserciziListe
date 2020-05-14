a5dd892120c6ba446b22c13c9ca52713749fe83a
Davide <davidegalante88@gmail.com>
2020-05-14 17:01:11 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 public class Main {
 
-// SCEMO CHI LEGGE
-//COMMENTO MIO 
 public static void main(String[] args) {
-	Lista l;
-	l = new Lista();
-	l.inserisciTesta("A");
-	l.inserisciTesta("B");
-	l.inserisciTesta("C");
-	l.inserisciTesta("D");
+	ListaDiInteri l;
+	l = new ListaDiInteri();
+	l.inserisciTesta(1);
+	l.inserisciTesta(2);
+	l.inserisciTesta(3);
+	l.inserisciTesta(4);
 	l.stampa();
 	
-	String s = l.eliminaInTesta();
+	int s = l.eliminaInTesta();
 	System.out.println("ho eliminato: "+s); 	//D
 	l.stampa();									//C B A
 	s = l.eliminaInTesta();
